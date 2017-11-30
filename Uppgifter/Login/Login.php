c1abed883f2f786ffaa1b75783a925d9c73e7bf1
imthatjeppe <jesper.johansson.te16c@gmail.com>
2017-11-30 15:25:14 +0100

    fixat till min låda

--- Uppgifter/Login/Login.php
+++ Uppgifter/Login/Login.php
@@ -8,14 +8,25 @@
 		<style>
 			
 			#Låda{
-			width:220px;
-			height:150px;
-			background-color: blue;
+			width:720px;
+			height:500px;
+			background-color: lightblue;
 			border: 2px solid #000;
+			margin-left: 440px;
 			}
 			
+			#Submit{
+			margin-left: 5px;
 			
-							
+			
+			}
+				
+
+			#Namn{
+				
+		
+				
+			}
 	
 		
 		
@@ -33,14 +44,16 @@
 	<body>
 		
 		<div id="Låda">
-		<form method="POST" action="Submit.php">
-		<label><b>Name</b></label><br>
-		<input type="text" placeholder="Write fullname" name="fullname" required><br>
-		<label><b>Password</b></label><br>
-		<input type="Password" placeholder="Password" name="Password" required><br>
-		<div class="Knappar"><br>
-		<button type="submit" class="signinbtn">Sign in</button>
-		<button type="button"  class="cancelbtn">Cancel</button>
+			<div id="Submit">
+				<form method="POST" action="Submit.php">
+				<label><b>Name</b></label><br>
+				<div id="Namn"><input type="text" placeholder="Write fullname" name="fullname" required><br></div>
+				<label><b>Password</b></label><br>
+				<input type="Password" placeholder="Password" name="Password" required><br>
+				<div class="Knappar"><br>
+				<button type="submit" class="signinbtn">Sign in</button>
+				<button type="button"  class="cancelbtn">Cancel</button>
+			</div>
 		</div>
 	</body>
 
