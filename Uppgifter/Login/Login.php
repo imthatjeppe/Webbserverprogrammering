550e94b0e9079d7a19fb41f7384a8850001eb45b
imthatjeppe <jesper.johansson.te16c@gmail.com>
2017-12-01 10:59:36 +0100

    Gjorde min knapp snyggare.
    Jag har kollat lite på internet efter lite saker som jag inte visste, Visste det mesta men några saker som t.ex content och cursor visste jag inte men nu vet jag.

--- Uppgifter/Login/Login.php
+++ Uppgifter/Login/Login.php
@@ -14,11 +14,12 @@
 			border: 2px solid #000;
 			margin-left: 440px;
 			font-size:28px;
+			
 			}
 			
 			#Submit{
 			margin-left: 37.5%;
-			margin-top: 25%;
+			margin-top: 5%;
 			
 			
 			
@@ -39,7 +40,38 @@
 			color:#FFFFFF;
 			text-align:center;
 			font-size:20px;
-		
+			padding:15px;
+			width:170px;
+			height:50px;
+			transition: all 0.5s;
+			cursor: pointer;
+			}
+			
+			.button span{
+			cursor: pointer;
+			display: inline-block;
+			position: relative;
+			transition: 0.5s;
+			}
+			
+			.button span:after {
+			content: '\00bb';
+			position: absolute;
+			opacity: 0;
+			top: 0;
+			right:-20px;
+			transition: 0.5s;
+			}
+			
+			.button:hover span{
+			padding-right: 25px;
+				
+			}
+			
+			.button:hover span:after{
+			opacity: 1;
+			right:0;
+			
 		</style>
 	
 	
